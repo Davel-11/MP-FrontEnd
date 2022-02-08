5276d5edaf7e0dc9a1201c3a03f80742fb568487
davelaltan@gmail.com <davelaltan@gmail.com>
2022-02-08 02:19:21 -0600

    front end up and ready to start creating components

---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.scss'
+
+const Main = () => {
+    const data = null
+
+    return <div className="main">
+        <h1>Gella </h1>
+    </div>
+}
+
+export default Main
